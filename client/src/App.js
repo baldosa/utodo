1b39b0616cf257eab2bd47cd208264b68081b489
Damian Mantuano <damianm@camba.coop>
2022-01-06 02:46:47 -0300

    Using craco to override webpack settings in order to import components from src/components folder

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Board from 'react-trello'
 import { Box } from '@mui/material';
-import MainMenu from 'components/mainMenu';
+import MainMenu from '@components/mainMenu';
+// import MainMenu from 'components/mainMenu';
 
 
 const data = {
@@ -16,18 +17,6 @@ const data = {
         {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
         {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
       ]
-    },
-    {
-      id: 'lane2',
-      title: 'Completed',
-      label: '0/0',
-      cards: []
-    },
-    {
-      id: 'lane2',
-      title: 'Completed',
-      label: '0/0',
-      cards: []
     },
     {
       id: 'lane2',
