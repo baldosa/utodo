d1980f8b6d2c3343e2be9dbd71b7abfb7fe6e613
Damian Mantuano <damianm@camba.coop>
2022-01-07 03:53:56 -0300

    added helmet and router dom

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,15 +1,16 @@
 const { when, whenDev, whenProd, whenTest, ESLINT_MODES, POSTCSS_MODES } = require("@craco/craco");
 const path = require("path");
+
 module.exports = {
-    reactScriptsVersion: "react-scripts" /* (default value) */,
+    reactScriptsVersion: "react-scripts",
     webpack: {
         configure: {
           resolve: {
-            extensions: ['js'],
             alias: {
               '@': path.resolve(__dirname, 'src'),
               '@assets': path.resolve(__dirname, 'src/components'),
               '@components': path.resolve(__dirname, 'src/components'),
+              '@views': path.resolve(__dirname, 'src/views'),
               // ...etc
             },
           }
