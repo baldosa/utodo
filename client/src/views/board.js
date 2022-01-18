6e37aa852d6e4a10e1130ca48465c53bd664cc11
Damian Mantuano <smarbos@gmail.com>
2022-01-18 18:40:42 -0300

    fixed something?

--- client/src/views/board.js
+++ client/src/views/board.js
@@ -1,27 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Container, Typography, Box, Fab } from '@mui/material';
-import Board from 'react-trello'
-import AddIcon from '@mui/icons-material/Add';
-import { Helmet } from "react-helmet";
-import EditIcon from '@mui/icons-material/Edit';
-import Tooltip from '@mui/material/Tooltip';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import ListItem from '@mui/material/ListItem';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import CloseIcon from '@mui/icons-material/Close';
-import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Slide from '@mui/material/Slide';
-
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-=======
 import { Container, Fab } from '@mui/material';
 import AsyncBoard from 'react-trello'
 import AddIcon from '@mui/icons-material/Add';
@@ -30,7 +7,6 @@ import { useSelector, useDispatch } from 'react-redux'
 import { usePrefetch } from '@services/board'
 import { useCallback, useEffect } from 'react'
 
->>>>>>> d8f8d85 (implemented state management with redux)
 
 // Override components here
 const components = {
